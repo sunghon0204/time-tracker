1d23e5e0e8245cc725cf63d59edde486d2a5f23b
sunghon0204 <53925626+sunghon0204@users.noreply.github.com>
2019-08-09 15:09:16 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -26,10 +26,6 @@ public class Tracker {
     public TimeEntry get(int index) {
         boolean valid = false;
 
-        if (valid == true) {
-            // whatever
-        }
-
         return entries.get(index);
     }
 }
