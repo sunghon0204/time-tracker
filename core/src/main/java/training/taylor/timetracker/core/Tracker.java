7c541e12e9eb68d0f2cb43eaa166437e2bf11d31
sunghon0204 <53925626+sunghon0204@users.noreply.github.com>
2019-08-09 15:19:59 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -23,7 +23,7 @@ public class Tracker {
         return entries.size();
     }
 
-    private TimeEntry get(int index) {
+    public TimeEntry get(int index) {
         return entries.get(index);
     }
 }
